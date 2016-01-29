4a5ef81f2159c31b8452f9d69e7d239e9aa87d49
王正一 <fsymbio@icloud.com>
2016-01-29 17:02:46 +0800

    添加访客视图

---
+++ SwiftWB/Classes/Views/Main/VisitorBase/VisitorView.swift
@@ -0,0 +1,21 @@
+//
+//  VisitorView.swift
+//  SwiftWB
+//
+//  Created by qufenqi on 16/1/29.
+//  Copyright © 2016年 王正一. All rights reserved.
+//
+
+import UIKit
+
+class VisitorView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
